e2ffa465ef176ed7acef8b4572b397cde89f4727
atul1104 <waghamareatul2@gmail.com>
2022-03-24 00:49:12 +0530

    Assignment

--- block-BNaadm/blog/app.js
+++ block-BNaadm/blog/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var articlesRouter = require('./routes/articles');
 const { default: mongoose } = require('mongoose');
+var commentsRouter = require('./routes/comments');
 
 //connect to mongodb
 mongoose.connect('mongodb://localhost/blog', (err) => {
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/articles', articlesRouter);
+app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
